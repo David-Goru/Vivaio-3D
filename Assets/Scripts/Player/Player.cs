cddc7f64f57025a32d5fe736fc9cd4e7cb916ca8
David Gómez <david.gomez.rooiz@gmail.com>
2021-08-17 17:42:05 +0200

    Player movement animations + look rotation fixed

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -43,22 +43,14 @@ public class Player
             return;
         }
 
-
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
-            string newAnimation = Input.GetButton("Run") ? "RUN" : "WALK";
-            if (lastAnimation != newAnimation)
-            {
-                playerObject.SetAnimation(newAnimation);
-                lastAnimation = newAnimation;
-            }
-
-            lastFrameMovement = Vector3.ClampMagnitude(Vector3.right * Input.GetAxis("Horizontal") + Vector3.forward * Input.GetAxis("Vertical"), 1.0f); // Clamp to avoid faster diagonal movement
-            lastFrameSpeed = Input.GetButton("Run") ? runSpeed : defaultSpeed;            
+            if (Input.GetButton("Run")) run();
+            else walk();         
         }
         else if (lastFrameMovement != Vector3.zero)
         {
-            playerObject.SetAnimation("IDLE");
+            setAnimation("IDLE");
             lastFrameMovement = Vector3.zero;            
         }
 
@@ -79,7 +71,7 @@ public class Player
         {
             Vector3 direction = lastFrameMovement * Time.deltaTime * lastFrameSpeed;
             playerObject.Translate(direction);
-            playerObject.LookAt(direction);
+            playerObject.LookAt(direction * 10);
         }
     }
 
@@ -91,7 +83,11 @@ public class Player
 
         if (Physics.Raycast(ray, out hit, 100, playerObject.PlowableLayer))
         {
-            if (CheckDistance(hit.point)) Plow(hit);
+            if (CheckDistance(hit.point))
+            {
+                lastAnimation = "PLOW";
+                Plow(hit);
+            }
         }
     }
 
@@ -111,6 +107,35 @@ public class Player
             blockMovement();
             playerObject.SetAnimation("PLOW");
         }
+    }
+
+    private void walk()
+    {
+        setAnimation("WALK");
+        lastFrameMovement = getLastFrameMovement();
+        
+        lastFrameSpeed = defaultSpeed;
+    }
+
+    private void run()
+    {
+        setAnimation("RUN");
+        lastFrameMovement = getLastFrameMovement();
+        lastFrameSpeed = runSpeed;
+    }
+
+    private Vector3 getLastFrameMovement()
+    {
+        Vector3 movementDirectionAndMagnitude = Vector3.right* Input.GetAxis("Horizontal") + Vector3.forward * Input.GetAxis("Vertical");
+        return Vector3.ClampMagnitude(movementDirectionAndMagnitude, 1.0f); // Clamp to avoid faster diagonal movement
+    }
+
+    private void setAnimation(string newAnimation)
+    {
+        if (lastAnimation == newAnimation) return;
+
+        lastAnimation = newAnimation;
+        playerObject.SetAnimation(newAnimation);
     }
 
     private void blockMovement()
